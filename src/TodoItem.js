41db5e3e182f24dabe3a6b97cab51403bca55a52
torvalds <torvalds@ekohe.com>
2019-05-10 19:27:18 +0800

    PropTypes DefaultProps

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
   constructor(props){
@@ -22,6 +23,16 @@ class TodoItem extends Component {
 
     deleteItem(index);
   }
+}
+
+TodoItem.propTypes = {
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired, // content 要么是 string, 要么是 nember.
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
+}
+
+TodoItem.defaultProps = {
+  content: 'Learing React'
 }
 
 export default TodoItem;
