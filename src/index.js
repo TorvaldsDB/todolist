0d7c6c7d890c841934cd7a7e8a036714cee5ad19
torvalds <torvalds@ekohe.com>
2019-05-26 19:13:45 +0800

    清空文件夹, 为 redux 准备

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Todolist from './Todolist';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Todolist />, document.getElementById('root'));
