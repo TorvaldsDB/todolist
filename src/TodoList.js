aba15e09facd2300e88b35a58a443fddc9ebe2e6
torvalds <torvalds@ekohe.com>
2019-05-27 00:27:08 +0800

    引入 redux测试工具, 实现 TodoList 新增功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store';
@@ -7,6 +7,11 @@ class TodoList extends Component {
   constructor(props){
     super(props);
     this.state = store.getState();
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange);
   }
 
   render() {
@@ -16,8 +21,12 @@ class TodoList extends Component {
           value={this.state.inputvalue}
           placeholder="todo info"
           style={{width: '300px', marginRight: '10px'}}
+          onChange={this.handleInputChange}
         />
-        <Button type="primary">Submit</Button>
+        <Button
+          type="primary"
+          onClick={this.handleBtnClick}
+        >Submit</Button>
         <List
           style={{marginTop: '10px', width: '300px'}}
           bordered
@@ -30,6 +39,27 @@ class TodoList extends Component {
         />
       </div>
     )
+  }
+
+  handleInputChange(e) {
+    const action = {
+      type: "change_input_value",
+      value: e.target.value
+    }
+
+    store.dispatch(action); // store dispatch action 的同时, 也把当前的 state 传递给了 reducer
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+
+  handleBtnClick() {
+    const action = {
+      type: "add_todo_item"
+    }
+
+    store.dispatch(action)
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,20 @@
 const defaultState = {
-  inputvalue: "123",
-  list: [1, 2]
+  inputvalue: "",
+  list: []
 }
 
+// reducer 可以接受 state, 但是绝不能修改 state
 export default (state = defaultState, action) => {
+  if (action.type === "change_input_value") {
+    const newState = JSON.parse(JSON.stringify(state)); // 对原来的数据进行一次深拷贝
+    newState.inputvalue = action.value
+    return newState;
+  }
+  if (action.type === "add_todo_item") {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(newState.inputvalue);
+    newState.inputvalue = "";
+    return newState;
+  }
   return state;
 }
