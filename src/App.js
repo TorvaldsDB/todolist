7a1835131b64217402e6bed3143df5baa5a414da
torvalds <torvalds@ekohe.com>
2019-05-26 18:41:40 +0800

    使用 react-transition-group 管理多个元素

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
 import React, { Component, Fragment } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './style.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true
+      list: []
     }
 
-    this.handleToggle = this.handleToggle.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
   }
 
   render () {
     return (
       <Fragment>
-
-        <CSSTransition
-          in={this.state.show}
-          timeout={1000}
-          classNames="fade"
-          unmountOnExit
-          onEntered={(el) => {el.style.color = 'blue'}}
-          appear={true}
-        >
-          <div>Hello</div>
-        </CSSTransition>
-        <button onClick={this.handleToggle}>toggle</button>
+        <TransitionGroup>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition
+                  timeout={1000}
+                  classNames="fade"
+                  unmountOnExit
+                  onEntered={(el) => {el.style.color = 'blue'}}
+                  appear={true}
+                  key={index}
+                >
+                  <div>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>toggle</button>
       </Fragment>
     )
   }
 
-  handleToggle() {
-    this.setState({
-      show: !this.state.show
+  handleAddItem() {
+    this.setState((prevState) => {
+      return {
+        list: [...prevState.list, 'item']
+      }
     })
   }
 }
