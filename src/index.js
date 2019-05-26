a0fed68ad4ba497c423d7f6330bba34533103589
torvalds <torvalds@ekohe.com>
2019-05-26 17:58:28 +0800

    React 中实现 CSS 过渡动画

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Todolist from './Todolist';
+import App from './App';
 
-ReactDOM.render(<Todolist />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
