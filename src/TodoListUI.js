8f0d2c5ea48a058c7e1d6a86732acbabf5712824
torvalds <torvalds@ekohe.com>
2019-06-09 23:03:09 +0800

    无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
-  render(){
-    return (
-      <div style={{marginTop: '50px', marginLeft: '20px'}}>
-        <Input
-          value={this.props.inputValue}
-          placeholder="todo info"
-          style={{width: '300px', marginRight: '10px'}}
-          onChange={this.props.handleInputChange}
-        />
-        <Button
-          type="primary"
-          onClick={this.props.handleBtnClick}
-        >Submit</Button>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={this.props.handleItemDelete.bind(index)}>
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
-    )
-  }
+const TodoListUI = (props) => {
+  return (
+    <div style={{marginTop: '50px', marginLeft: '20px'}}>
+      <Input
+        value={props.inputValue}
+        placeholder="todo info"
+        style={{width: '300px', marginRight: '10px'}}
+        onChange={props.handleInputChange}
+      />
+      <Button
+        type="primary"
+        onClick={props.handleBtnClick}
+      >Submit</Button>
+      <List
+        style={{marginTop: '10px', width: '300px'}}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (
+          <List.Item onClick={props.handleItemDelete.bind(index)}>
+            {item}
+          </List.Item>
+        )}
+      />
+    </div>
+  )
 }
 
 export default TodoListUI;
