b1dbaa474c957c46461703f061f4982120b9408e
torvalds <torvalds@ekohe.com>
2019-08-31 21:02:56 +0800

    6.4 - 在中间件中使用 redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk)
- );
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
+const store = createStore(reducer, enhancer);
 
 export default store;
